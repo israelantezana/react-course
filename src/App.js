633afc160a2ac003bfaca0696a0be3457d373725
Israel Antezana Rojas <israelantezana@gmail.com>
2021-07-02 17:48:38 -0400

    Building & Reusing Another Component 47:50 -- Creando componente Todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
