ea5be1d419a61dedf1dc531601278e8aa7ea7de5
Israel Antezana Rojas <israelantezana@gmail.com>
2021-07-02 19:04:29 -0400

    Introducing State 1:12:48 - Abrir el modal solo cuando se requiere

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState();
   function deleteHandler() {
-    console.log("Clicked");
-    console.log(props.text);
+    setModalIsOpen(true);
   }
 
   return (
@@ -12,6 +16,8 @@ function Todo(props) {
           Delete
         </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
